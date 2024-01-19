103b4e250f59077d8db887db642c99d13eb2dd9d
infiniteltd <chrissamps24@gmail.com>
2024-01-19 17:39:39 +0100

    added component functions and new dependencies

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -4,17 +4,41 @@ import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typings";
 import PortableText from "react-portable-text";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { useState } from "react";
 
+interface IFormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
 interface Props {
   post: Post;
 }
 
 function Post({ post }: Props) {
+  const [submitted, setSubmitted] = useState(false);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<IFormInput>();
+
+  const onSubmitHandler: SubmitHandler<IFormInput> = async (data) => {
+    await fetch("/api/createComment", {
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        console.log(data);
+        setSubmitted(true);
+      })
+      .catch((err) => {
+        console.log(err);
+        setSubmitted(false);
+      });
+  };
 
   return (
     <main>
@@ -73,38 +97,96 @@ function Post({ post }: Props) {
 
       <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
 
-      <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
-        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
-        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
-        <hr className="py-3 mt-2" />
+      {submitted ? (
+        <div className="flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto">
+          <h3 className="text-3xl font-bold">
+            Thank you for submitting your comment!
+          </h3>
+          <p>Once it has been approved, it will appear below!</p>
+        </div>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmitHandler)}
+          className="flex flex-col p-5 max-w-2xl mx-auto mb-10"
+        >
+          <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+          <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+          <hr className="py-3 mt-2" />
 
-        <label className="black mb-5">
-          <span className="text-gray-500">Name</span>
           <input
-            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-            placeholder="John Doe"
-            type="text"
+            {...register("_id")}
+            type="hidden"
+            name="_id"
+            value={post._id}
           />
-        </label>
 
-        <label className="black mb-5">
-          <span className="text-gray-500">Email</span>
+          <label className="black mb-5">
+            <span className="text-gray-500">Name</span>
+            <input
+              {...register("name", { required: true })}
+              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+              placeholder="John Doe"
+              type="text"
+            />
+          </label>
+
+          <label className="black mb-5">
+            <span className="text-gray-500">Email</span>
+            <input
+              {...register("email", { required: true })}
+              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+              placeholder="johnDoe@email.com"
+              type="email"
+            />
+          </label>
+
+          <label className="black mb-5">
+            <span className="text-gray-500">Comment</span>
+            <textarea
+              {...register("comment", { required: true })}
+              className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+              placeholder="Type your comment here"
+              rows={8}
+            />
+          </label>
+
+          {/* errors will return when field validation fails */}
+          <div className="flex flex-col p-5">
+            {errors.name && (
+              <span className="text-red-500">The Name field is required</span>
+            )}
+            {errors.email && (
+              <span className="text-red-500">The Email field is required</span>
+            )}
+            {errors.comment && (
+              <span className="text-red-500">
+                The Comment field is required
+              </span>
+            )}
+          </div>
+
           <input
-            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-            placeholder="johnDoe@email.com"
-            type="email"
+            type="submit"
+            className="shadow bg-yellow-500 hover:bg-yellow-400 focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer"
           />
-        </label>
+        </form>
+      )}
 
-        <label className="black mb-5">
-          <span className="text-gray-500">Comment</span>
-          <textarea
-            className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-            placeholder="Type your comment here"
-            rows={8}
-          />
-        </label>
-      </form>
+      {/* Comments */}
+      <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
+        <h3 className="text-4xl">Comments </h3>
+        <hr className="pb-2" />
+
+        {post.comments.map((comment) => (
+          <div key={comment._id}>
+            <p>
+              <span className="text-yellow-500">{comment.name}:</span>
+              {"  "}
+              {comment.comment}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
@@ -142,6 +224,11 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
         name,
         image
       },
+      "comments": *[
+        _type == "comment" &&
+        post._ref == ^._id && 
+        approved == true
+        ],  
         description,
         mainImage,
         slug,
